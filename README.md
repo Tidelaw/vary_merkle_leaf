7764f0048ac37f79ad67061efb723dc2b9a4e923
hagi <t1d3law@gmail.com>
2024-01-05 20:47:21 +0700

    basic code and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+run using npx ts-node index.ts
+
+This project seeks to find out:
+
+How can block sizes of a Merkle tree's hashed leaf nodes be optimised as a function of the unreliability of the channel in order to maximise net speed in data verification?
+
+An image will be used in order to mimic the file, its partitions being compared in terms of speed.
--- index.ts
+++ index.ts
@@ -10,11 +10,7 @@ const tree = new MerkleTree(leaves, SHA256)
 const root = tree.getRoot().toString('hex')
 const leaf = SHA256('a')
 const proof = tree.getProof(leaf)
-console.log(tree.verify(proof, leaf, root)) // true
 
+console.log(tree.verify(proof, leaf, root))
 
-const badLeaves = ['a', 'x', 'c'].map(x => SHA256(x))
-const badTree = new MerkleTree(badLeaves, SHA256)
-const badLeaf = SHA256('x')
-const badProof = badTree.getProof(badLeaf)
-console.log(badTree.verify(badProof, badLeaf, root)) // false
+console.log(root)
