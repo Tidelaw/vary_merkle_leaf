8968f95437e13d25828297a85916bd9361921217
hagi <t1d3law@gmail.com>
2024-04-15 08:00:16 +0700

    partitions and time taken

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ run using npx ts-node index.ts
 
 This project seeks to find out:
 
-How can block sizes of a Merkle tree's hashed leaf nodes be optimised as a function of the unreliability of the channel in order to maximise net speed in data verification?
+How can block sizes of a Merkle tree's hashed leaf nodes be optimised as a function of the unreliability of the channel in order to maximise speed in data verification?
 
-An image will be used in order to mimic the file, its partitions being compared in terms of speed.
+An image will be used in order to mimic the file, its hashed partitions making up the merkle tree
--- index.ts
+++ index.ts
@@ -1,16 +1,47 @@
-const { MerkleTree } = require('merkletreejs')
-const SHA256 = require('crypto-js/sha256')
+const { MerkleTree } = require("merkletreejs");
+const SHA256 = require("crypto-js/sha256");
 
+// make partitions
+function partitionString(string: any, numPartitions: any) {
+    if (numPartitions <= 0) {
+        return ["Invalid number of partitions"];
+    }
 
-// this will be what is changed, sizes of leaves will be varied (partitions of the image)
-const leaves = ['a', 'b', 'c'].map(x => SHA256(x))
+    const partitionSize = Math.ceil(string.length / numPartitions);
+    const partitions: any = [];
 
+    for (let i = 0; i < string.length; i += partitionSize) {
+        partitions.push(string.substring(i, i + partitionSize));
+    }
 
-const tree = new MerkleTree(leaves, SHA256)
-const root = tree.getRoot().toString('hex')
-const leaf = SHA256('a')
-const proof = tree.getProof(leaf)
+    // Handling the case where the last partition might be smaller
+    if (partitions.length > numPartitions) {
+        partitions[partitions.length - 1] += partitions.pop();
+    }
 
-console.log(tree.verify(proof, leaf, root))
+    return partitions;
+}
 
-console.log(root)
+function main() {
+    // this will be what is changed, sizes of leaves will be varied (partitions of the image)
+    const bs64_tides = require("fs").readFileSync("base64_tides.txt", "utf8");
+    console.log(bs64_tides.length);
+    // const leaves = ['a', 'b', 'c', 'd', 'e', 'f'].map(x => SHA256(x))
+    const partitions = partitionString(bs64_tides, 64)
+
+    console.log(partitions, partitions.length)
+
+    const start = performance.now();
+    const tree = new MerkleTree(partitions, SHA256);
+    const end = performance.now();
+    const root = tree.getRoot().toString("hex");
+    console.log(end-start)
+    // const leaf = SHA256("a");
+    // const proof = tree.getProof(leaf);
+
+    // console.log(tree.verify(proof, leaf, root), root, proof, "q");
+
+    // console.log(root, "2");
+}
+
+main()
