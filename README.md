8163837ead84cc12db123bc7a306eaa3823e666a
hagi <t1d3law@gmail.com>
2024-05-08 13:13:42 +0700

    complete suboptimal solution

--- README.md
+++ README.md
@@ -15,4 +15,10 @@ Solve by querying for the same block and proof each time if the block transferre
 
 3. More partitions or smaller block size always leads to exponentially slower net speed
 
-Probably because partitions are being hashed and thus when simulating calculating speed of data transfer, hash sizes are fixed and thus always result in a longer duration. The solution is to not map over the initial partitions with SHA256. 
+    1. Probably because partitions are being hashed and thus when simulating calculating speed of data transfer, hash sizes are fixed and thus always result in a longer duration. The solution is to not map over the initial partitions with SHA256. 
+
+    2. Size of leaves is incorrect, one example of leaf vs proof size is 14.4 44887. Issue was still using the leaves of the corrupted merkle tree instead of just sending the partitions since the leaves are standardized at the SHA256 hash size.
+
+4. Scaling file size
+
+5. Recovering corrupted file blocks with one Merkle proof per corrupted block
--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
 const { MerkleTree } = require("merkletreejs");
 const SHA256 = require("crypto-js/sha256");
-let simulatedTime:any = 0;
-const unreliability = 0.99;
+let simulatedTime: any = 0;
+const unreliability = 0.66;
+const charSim = 3 * Math.pow(10, -3)
+
+// with this many characters, 333k, the file being transferred is 333kb
+// however, to scale this as some more understandable standards e.g a 1tb file at 1gb/s which would perfectly take 1000s to transfer
+// to scale the file, multiply by 3x10^6, what does this mean for time and speed though?
+// the speed stays constant however we can calculate an amount of time we should increment when simulating data transfer
+// e.g, (blocksize*3x10^6)/t = 1x10^9
+// (blocksize*3x10^6)/10^9 = t, thus for a given block size, we can multiply it by 3*10^-3.
 
 // this will be what is changed, sizes of leaves will be varied (partitions of the image)
 const bs64_tides = require("fs").readFileSync("base64_tides.txt", "utf8");
@@ -29,7 +37,7 @@ function mimicNetworkUnreliability(file: any) {
 
   if (random < unreliability) {
     // simply make corruption as impossible hash
-    final = SHA256("A")
+    final = file.substr(0, file.length - 1);
   } else {
     final = file;
   }
@@ -109,55 +117,71 @@ function partitionString(string: any, numPartitions: any) {
 //   return null;
 // }
 
-
-
-function requeryCorrupted(tree:any, workingTree:any, originalRoot:any) {
-  let corruptLeaves:any = []
+function requeryCorrupted(
+  tree: any,
+  workingTree: any,
+  corruptedPartitions: any,
+  originalRoot: any
+) {
   const leaves = tree.getLeaves();
-  const workingLeaves = workingTree.getLeaves();
-
-  for (let i = 0; i < workingLeaves.length; i++) {
-    const leaf = workingLeaves[i];
+  const corrupted = corruptedPartitions;
+  for (let i = 0; i < corrupted.length; i++) {
+    const leaf = corrupted[i];
     // const workingProof = workingTree.getProof(leaf);
-    const proof = tree.getProof(leaves[i])
+    const proof = tree.getProof(leaves[i]);
     // simulatedTime += Number(JSON.stringify(workingProof).length/10)
     const isValidLeaf = tree.verify(proof, leaf, originalRoot);
     // delay here should be counting size of both proof and the block, add in below
 
     if (!isValidLeaf) {
       let random = Math.random();
-    
+
       while (random < unreliability) {
-        simulatedTime += Number((JSON.stringify(leaf).length/10)+JSON.stringify(proof).length/10)
-        random = Math.random()
+        // console.log(new TextEncoder().encode(JSON.stringify(leaf)).length)
+
+        // console.log((corruptedPartitions), JSON.stringify(proof).length/10, '00')
+        // console.log(leaf, JSON.stringify(leaf).length)
+        // console.log(corruptedPartitions[i], corruptedPartitions[i].length, JSON.stringify(corruptedPartitions[i]).length)
+        simulatedTime += Number(
+          JSON.stringify(leaf).length*(charSim)+ JSON.stringify(proof).length*(charSim)
+        );
+        random = Math.random();
       }
-    }
-    else {
-      workingLeaves[i]=leaves[i]
+    } else {
+      corrupted[i] = leaves[i];
     }
   }
-  const finalTree = new MerkleTree(workingLeaves, SHA256)
+  const finalTree = new MerkleTree(corrupted, SHA256);
   return finalTree.getRoot().toString("hex");
 }
 
 // issue arised, more partitions or smaller block size is always exponentially slower
 // probably because parititons are being hashed and thus when simulating calculating speed of data transfer, it is always more.
 
-function testing() {
+function testingPowers() {
   for (let i = 1; i <= 12; i++) {
     const numPartitions = Math.pow(2, i);
     // const numPartitions = 4096
+    const start = performance.now();
     const partitions = partitionString(bs64_tides, numPartitions);
     const tree = new MerkleTree(partitions, SHA256);
 
-    const corruptedPartitions = partitions.map((x: any) => mimicNetworkUnreliability(x));
+    const corruptedPartitions = partitions.map((x: any) =>
+      mimicNetworkUnreliability(x)
+    );
 
     const root = tree.getRoot().toString("hex");
-    
+
     const workingTree = new MerkleTree(corruptedPartitions, SHA256);
     const workingRoot = workingTree.getRoot().toString("hex");
 
-    let finalRoot = requeryCorrupted(tree, workingTree, root)
+    let finalRoot = requeryCorrupted(
+      tree,
+      workingTree,
+      corruptedPartitions,
+      root
+    );
+    const end = performance.now();
 
     // if (root==workingRoot){ fix later
     //   console.log("success!")
@@ -165,10 +189,40 @@ function testing() {
     // else {
     //   console.log(root, finalRoot, 'fail')
     // }
-    // console.log(end - start, workingRoot, root);
+    // console.log(end - start, 'setup');
     // actual end-start time is mostly due to hashing/merkle reconstructing times and can therefore be 'controlled'
-    console.log(simulatedTime)
+    console.log(simulatedTime);
   }
 }
 
-testing();
+testingPowers();
+
+// function testingLinear() {
+//   for (let i = 1; i <= 128; i++) {
+//     const numPartitions = i;
+//     // const numPartitions = 4096
+//     const partitions = partitionString(bs64_tides, numPartitions);
+//     const tree = new MerkleTree(partitions, SHA256);
+
+//     const corruptedPartitions = partitions.map((x: any) => mimicNetworkUnreliability(x));
+
+//     const root = tree.getRoot().toString("hex");
+
+//     const workingTree = new MerkleTree(corruptedPartitions, SHA256);
+//     const workingRoot = workingTree.getRoot().toString("hex");
+
+//     let finalRoot = requeryCorrupted(tree, workingTree, root)
+
+//     // if (root==workingRoot){ fix later
+//     //   console.log("success!")
+//     // }
+//     // else {
+//     //   console.log(root, finalRoot, 'fail')
+//     // }
+//     // console.log(end - start, workingRoot, root);
+//     // actual end-start time is mostly due to hashing/merkle reconstructing times and can therefore be 'controlled'
+//     console.log(simulatedTime)
+//   }
+// }
+
+// testingLinear();
